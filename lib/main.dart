eaa3be805f85f84c3050aaf390d38b4651a75c32
ProjectMgc2024 <projectmailmg@gmail.com>
2024-11-27 21:13:16 +0530

    new2.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/user/screens/home_screen.dart';
+import 'package:flutter_application_1/user/screens/intro.dart';
 import 'package:flutter_application_1/user/screens/introduction.dart';
 import 'package:flutter_application_1/user/screens/navigation.dart';
 import 'package:flutter_application_1/user/screens/second.dart';
 
 void main() {
-  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: HomeScreen()));
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: Introscreen()));
 }
